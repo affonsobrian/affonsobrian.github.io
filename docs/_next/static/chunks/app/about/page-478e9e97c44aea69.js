48ec4b987ec57f7ef9108ff3dae905c35c7c8d05
Affonso Brian <contato.affonsobrian@gmail.com>
2023-12-14 17:12:43 -0300

    new version

---
+++ docs/_next/static/chunks/app/about/page-478e9e97c44aea69.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{3524:function(e,r,t){Promise.resolve().then(t.bind(t,5470))},5470:function(e,r,t){"use strict";t.r(r);var n=t(9268);r.default=function(){return(0,n.jsx)("h1",{children:"Add about here"})}},3177:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(6006),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,_={key:!0,ref:!0,__self:!0,__source:!0};function c(e,r,t){var n,u={},c=null,i=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(i=r.ref),r)f.call(r,n)&&!_.hasOwnProperty(n)&&(u[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===u[n]&&(u[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:i,props:u,_owner:s.current}}r.Fragment=u,r.jsx=c,r.jsxs=c},9268:function(e,r,t){"use strict";e.exports=t(3177)}},function(e){e.O(0,[253,698,744],function(){return e(e.s=3524)}),_N_E=e.O()}]);
